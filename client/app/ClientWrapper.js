dc71eb8410571a7bcc0ea7cc18c8c33d04352f0c
Dip <dipkorimon@gmail.com>
2025-07-10 07:14:45 +0600

    refactor(app): update the client wrapper
    - There is no need to hide navbar or sidebar for login, register, etc. pages

--- client/app/ClientWrapper.js
+++ client/app/ClientWrapper.js
@@ -1,27 +1,20 @@
 "use client";
 
-import { usePathname } from "next/navigation";
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
 import { MessageProvider } from '@/contexts/MessageContext';
 import MessageBox from "@/components/MessageBox";
 
 export default function ClientWrapper({ children }) {
-    const pathname = usePathname();
-
-    // Hide Navbar on these paths
-    const hideNavbar =
-        pathname === "/auth/login" || pathname === "/auth/register" || pathname === "/auth/password-reset" || pathname === "/auth/password-reset-confirm";
 
     return (
         <MessageProvider>
             <MessageBox />
             <div className="flex h-screen overflow-hidden">
-                {!hideNavbar && <Sidebar />}
-
+                <Sidebar />
                 <div className="flex flex-col flex-1">
-                    {!hideNavbar && <Navbar />}
-                    <main className="flex-1 overflow-y-auto bg-gray-50 p-6">
+                    <Navbar />
+                    <main className="flex-1 overflow-y-auto bg-gray-50">
                         {children}
                     </main>
                 </div>
