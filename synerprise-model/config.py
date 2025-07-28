93c0aed1c2aeebc32b0d00604d3c4efafe075d86
Dip <dipkorimon@gmail.com>
2025-07-28 07:02:02 +0600

    chore(config): Update MAX_INPUT_LEN, MAX_TARGET_LEN - MAX_INPUT_LEN set to 12 for phonetic model input compatibility - MAX_TARGET_LEN updated to 10 reflecting decoder output length

--- synerprise-model/config.py
+++ synerprise-model/config.py
@@ -2,6 +2,6 @@
 default_model = "synerprise-phonetic"  # fallback model if detection fails
 
 # You can also add other global constants here if needed
-MAX_INPUT_LEN = 7
-MAX_TARGET_LEN = 30
+MAX_INPUT_LEN = 12
+MAX_TARGET_LEN = 10
 LATENT_DIM = 256
